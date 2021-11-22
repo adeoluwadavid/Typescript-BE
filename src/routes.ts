313b7d3daf8849ffe52d936ff19ca8268763c497
David Adewole <adeoluwadavid@gmail.com>
2021-11-22 08:56:53 +0100

    typesecript-node

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import validateResource from './middleware/validateResource'
 import { createUserSchema } from "./schema/user.schema";
 import {  createSessionSchema  } from "./schema/session.schema";
 import requireUser from "./middleware/requireUser";
+import { createProductSchema, updateProductSchema, getProductSchema, deleteProductSchema } from "./schema/product.schema";
+import { createProductHandler , updateProductHandler, getProductHandler, deleteProductHandler} from "./controller/product.contoller";
 
 export default function(app:Express){
     app.get('/healthcheck',(req:Request, res:Response)=> res.sendStatus(200));
@@ -21,4 +23,9 @@ export default function(app:Express){
     //Logout
     app.delete("/api/sessions", requireUser, deleteSessionHandler)
 
+    //Product
+    app.post('/api/products',[requireUser, validateResource(createProductSchema)], createProductHandler)
+    app.put('/api/products/:productId',[requireUser, validateResource(updateProductSchema)], updateProductHandler)
+    app.get('/api/products/:productId',[validateResource(getProductSchema)], getProductHandler)
+    app.delete('/api/products/:productId',[requireUser, validateResource(deleteProductSchema)], deleteProductHandler)
 }
